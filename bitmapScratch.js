de1cabeeb3da97d5370696777e67efc2eb80c4a1
haanj <joshua.haan@ymail.com>
2016-03-07 12:00:25 -0800

    moved color code read/write into separate functions

--- bitmapScratch.js
+++ bitmapScratch.js
@@ -7,10 +7,10 @@ let bitmap = fs.readFileSync(__dirname + '/' + process.argv[2]);
 let bitmapData = {};
 
 // bitmapData.headerField = bitmap.toString('ascii', 0, 2);
-bitmapData.fileSize = bitmap.readUInt32LE(2); // maybe ditch, might not need
+// bitmapData.fileSize = bitmap.readUInt32LE(2); // maybe ditch, might not need
 bitmapData.pixelArrayStart = bitmap.readUInt32LE(10);
-bitmapData.colorDepth = bitmap.readUInt32LE(28);
-bitmapData.compressionMethod = bitmap.readUInt32LE(30);
+// bitmapData.colorDepth = bitmap.readUInt32LE(28);
+// bitmapData.compressionMethod = bitmap.readUInt32LE(30);
 // bitmapData.imageSize = bitmap.readUInt32LE(34);
 // bitmapData.imageWidth = bitmap.readUInt32LE(18);
 // bitmapData.paletteColors = bitmap.readUInt32LE(46);
--- index.js
+++ index.js
@@ -28,20 +28,27 @@ function changeColorCodes(bitmap, bitmapData) {
   let transform = controller();
 
   for (let i = bitmapData.pixelArrayStart; i < bitmap.length - 3; i = i + 3) {
-    let colorCodes = {};
-    colorCodes.blue = bitmap.readUInt8(i);
-    colorCodes.green = bitmap.readUInt8(i + 1);
-    colorCodes.red = bitmap.readUInt8(i + 2);
-
+    let colorCodes = readColorCodes(bitmap, i);
     transform(colorCodes);
-
-    bitmap.writeUInt8(colorCodes.blue, i);
-    bitmap.writeUInt8(colorCodes.green, i + 1);
-    bitmap.writeUInt8(colorCodes.red, i + 2);
+    writeColorCodes(bitmap, colorCodes, i);
   }
+
   outputImage(bitmap);
 }
 
+function readColorCodes(bitmap, i) {
+  let colorCodes = {};
+  colorCodes.blue = bitmap.readUInt8(i);
+  colorCodes.green = bitmap.readUInt8(i + 1);
+  colorCodes.red = bitmap.readUInt8(i + 2);
+  return colorCodes;
+}
+
+function writeColorCodes(bitmap, colorCodes, i){
+  bitmap.writeUInt8(colorCodes.blue, i);
+  bitmap.writeUInt8(colorCodes.green, i + 1);
+  bitmap.writeUInt8(colorCodes.red, i + 2);
+}
 
 function outputImage(bitmap) {
   console.log('Printing new image');
