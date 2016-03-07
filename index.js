361a6c88a4a2e281608a403c1c298f8c0f9cd410
haanj <joshua.haan@ymail.com>
2016-03-07 12:19:00 -0800

    is now asyncronous

--- index.js
+++ index.js
@@ -3,19 +3,14 @@ const fs = require('fs');
 // const controller = require(__dirname + '/lib/controller');
 const colorHandler = require(__dirname + '/lib/colorHandler');
 
-
 function start(){
   let imagePath = (__dirname + '/' + process.argv[2]);
-
-  let bitmap = fs.readFileSync(imagePath);
-  let bitmapData = getBitmapData(bitmap);
-  colorHandler(bitmap, bitmapData);
+  fs.readFile(imagePath, (err, bitmap) => {
+    if (err) throw err;
+    let bitmapData = getBitmapData(bitmap);
+    colorHandler(bitmap, bitmapData);
+  });
 }
-
-// fs.readFile('/etc/passwd', (err, data) => {
-//   if (err) throw err;
-//   console.log(data);
-// });
 
 function getBitmapData(bitmap) {
   let bitmapData = {};
@@ -24,6 +19,5 @@ function getBitmapData(bitmap) {
   bitmapData.colorDepth = bitmap.readUInt32LE(28);
   return bitmapData;
 }
-
 
 start();
